2d0a03f2ff26fe9a0182471143a52e80963c5add
pitbull90 <henrycordero@live.com>
2019-07-09 20:23:22 -0700

    Added Skycons to weather app

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ window.addEventListener("load", () => {
   let temperatureDegree = document.querySelector(".temperature-degree");
   let locationTimeZone = document.querySelector(".location-timezone");
 
+  let temperatureSection = document.querySelector(".degree-section");
+  const tempratureSpan = document.querySelector(".degree-section span");
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
       const proxy = "http://cors-anywhere.herokuapp.com/";
-      const API = `${proxy}https://api.darksky.net/forecast/API_KEY/${lat},${long}`;
+      const API = `${proxy}https://api.darksky.net/forecast/52fca1849f496f685550dfb6cd771dc5/${lat},${long}`;
 
       fetch(API)
         .then(response => {
@@ -21,14 +24,37 @@ window.addEventListener("load", () => {
         })
         .then(data => {
           console.log(data);
-          const { temperature, summary } = data.currently;
+          const { temperature, summary, icon } = data.currently;
           // Set DOM Elements from the API
           temperatureDegree.textContent = temperature;
           temperatureDescription.textContent = summary;
           locationTimeZone.textContent = data.timezone;
+          // Formula for Celsius
+          let celsius = (temperature - 32) * (5 / 9);
+
+          // Set Icon
+          setIcons(icon, document.querySelector(".icon"));
+
+          // Change temperature to Celsius/Fahrenheit
+          temperatureSection.addEventListener("click", () => {
+            if (tempratureSpan.textContent === "F") {
+              tempratureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              tempratureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
         });
     });
   } else {
     h1.textContent = "Could not Access your Geolocation.";
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
